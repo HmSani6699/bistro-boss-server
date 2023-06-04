3870b624b516e1ff864340f147b6c875b7bfe85b
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-04 21:43:31 +0600

    filter the admin user in mongodb

--- index.js
+++ index.js
@@ -37,12 +37,16 @@ async function run() {
     //--------------------//
     //     USERS COLLATION  //
     //--------------------//
+
+    app.get('/users', async (req, res) => {
+      const result = await userCollaction.find().toArray();
+      res.send(result)
+    })
+
     app.post('/user', async (req, res) => {
       const user = req.body;
-      console.log(user);
       const query = { email: user.email };
       const existingUser = await userCollaction.findOne(query);
-      console.log(existingUser);
       if (existingUser) {
         return {}
       }
@@ -50,6 +54,17 @@ async function run() {
       res.send(result)
     })
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const updateDoc = {
+        $set: {
+          rol: `admin`
+        },
+      };
+      const result = await userCollaction.updateOne(filter, updateDoc);
+      res.send(result);
+    })
 
     //--------------------//
     //     MENU COLLATION  //
