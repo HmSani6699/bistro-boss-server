006315adef3b2d297575a251f93982918bc3ab56
H.M Sadiq <hmsadikurrahman120@gmail.com>
2023-06-05 00:17:53 +0600

    reacp the jwt api

--- index.js
+++ index.js
@@ -37,12 +37,10 @@ async function run() {
     //--------------------//
     // JWT //
     //--------------------//
-    app.post('/jwt', (req, res) => {
-      const user = req.body;
-      const token = jwt.sign({
-        data: user
-      }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-      res.send(token)
+    app.post('/jwt',(req,res)=>{
+         const user= req.body;
+         const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn: '1h' })
+         res.send({token})
     })
 
 
